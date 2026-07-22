51caa3cb190948c507b95872de3dd3f35aa9afb1
XeniaMonika <m.x.kudela@gmail.com>
2026-07-22 17:43:34 +0200

    Start mapping author to title for the lexical corpus

---
+++ Scripts/build_lexical_corpus.py
@@ -0,0 +1,7 @@
+import rdflib
+
+corpus_big_path = "C:\\Users\\kudelamo\\Projects\\Annif\\Data\\Spanish_ALL\\corpus.jsonl"
+corpus_small_path = "C:\\Users\\kudelamo\\Projects\\Annif\\Data\\Spanish_FID\\corpus.jsonl"
+mapped_keywords_path = "C:\\Users\\kudelamo\\Projects\\Annif\\Data\\Vocabs\\keywords_mapped_both.json"
+output_folder_big = "C:\\Users\\kudelamo\\Projects\\Annif\\Data\\Spanish_ALL\\Corpus_lexical"
+output_folder_small = "C:\\Users\\kudelamo\\Projects\\Annif\\Data\\Spanish_FID\\Corpus_lexical"
